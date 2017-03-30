cf3bdbabfa058dd7fd6c923205c74a0f8040016a
Robert Knight <robertknight@gmail.com>
2017-03-30 16:15:28 +0100

    Use `connectStore` to connect `<selection-tabs>` to the store

--- src/sidebar/components/selection-tabs.js
+++ src/sidebar/components/selection-tabs.js
@@ -1,42 +1,52 @@
 'use strict';
 
+var tabs = require('../tabs');
+var session = require('../reducers/session');
 var uiConstants = require('../ui-constants');
 
+function mapStateToController(state, dispatch) {
+  var counts = tabs.counts(state.annotations, true /* separateOrphans */);
+
+  return {
+    orphansTabFlagEnabled: session.isFeatureEnabled(state, 'orphans_tab'),
+    isWaitingToAnchorAnnotations: counts.anchoring > 0,
+    selectedTab: state.selectedTab,
+    totalAnnotations: counts.annotations,
+    totalNotes: counts.notes,
+    totalOrphans: counts.orphans,
+
+    selectTab: function (type) {
+      dispatch({type: 'CLEAR_SELECTION'});
+      dispatch({type: 'SELECT_TAB', tab: type});
+    },
+  };
+}
+
+//@ngInject
+function SelectionTabsController($element, connectStore) {
+  connectStore(this, mapStateToController);
+
+  this.TAB_ANNOTATIONS = uiConstants.TAB_ANNOTATIONS;
+  this.TAB_NOTES = uiConstants.TAB_NOTES;
+  this.TAB_ORPHANS = uiConstants.TAB_ORPHANS;
+
+  this.showAnnotationsUnavailableMessage = function () {
+    return this.selectedTab === this.TAB_ANNOTATIONS &&
+      this.totalAnnotations === 0 &&
+      !this.isWaitingToAnchorAnnotations;
+  };
+
+  this.showNotesUnavailableMessage = function () {
+    return this.selectedTab === this.TAB_NOTES &&
+      this.totalNotes === 0;
+  };
+}
+
 module.exports = {
+  controller: SelectionTabsController,
   controllerAs: 'vm',
-  //@ngInject
-  controller: function ($element, annotationUI, features) {
-    this.TAB_ANNOTATIONS = uiConstants.TAB_ANNOTATIONS;
-    this.TAB_NOTES = uiConstants.TAB_NOTES;
-    this.TAB_ORPHANS = uiConstants.TAB_ORPHANS;
-
-    this.selectTab = function (type) {
-      annotationUI.clearSelectedAnnotations();
-      annotationUI.selectTab(type);
-    };
-
-    this.orphansTabFlagEnabled = function () {
-      return features.flagEnabled('orphans_tab');
-    };
-
-    this.showAnnotationsUnavailableMessage = function () {
-      return this.selectedTab === this.TAB_ANNOTATIONS &&
-        this.totalAnnotations === 0 &&
-        !this.isWaitingToAnchorAnnotations;
-    };
-
-    this.showNotesUnavailableMessage = function () {
-      return this.selectedTab === this.TAB_NOTES &&
-        this.totalNotes === 0;
-    };
-  },
   bindings: {
     isLoading: '<',
-    isWaitingToAnchorAnnotations: '<',
-    selectedTab: '<',
-    totalAnnotations: '<',
-    totalNotes: '<',
-    totalOrphans: '<',
   },
   template: require('../templates/selection_tabs.html'),
 };
--- src/sidebar/components/test/selection-tabs-test.js
+++ src/sidebar/components/test/selection-tabs-test.js
@@ -2,62 +2,100 @@
 
 var angular = require('angular');
 
+var fakeConnectStore = require('../../test/fake-connect-store');
+
 var util = require('../../directive/test/util');
 
 describe('selectionTabs', function () {
+  var connectStore;
+
   before(function () {
+    var component = require('../selection-tabs');
+
+    // Simplify testing by making tabs activate on click rather than touch
+    component = Object.assign({}, component, {
+      template: component.template.replace('h-on-touch', 'ng-click'),
+    });
+
     angular.module('app', [])
-      .component('selectionTabs', require('../selection-tabs'));
+      .component('selectionTabs', component);
   });
 
   beforeEach(function () {
-    var fakeAnnotationUI = {};
-    var fakeFeatures = {
-      flagEnabled: sinon.stub().returns(true),
-    };
+    connectStore = fakeConnectStore({
+      annotations: [],
+      selectedTab: 'annotation',
+      session: {
+        features: {
+          'orphans_tab': true,
+        },
+      },
+    });
 
     angular.mock.module('app', {
-      annotationUI: fakeAnnotationUI,
-      features: fakeFeatures,
+      connectStore: connectStore,
     });
   });
 
+  var ann = {
+    $orphan: false,
+    target: [{
+      selector: [{}],
+    }],
+  };
+
+  var pageNote = {
+    $orphan: false,
+    target: [{}],
+  };
+
   context('displays selection tabs, counts and a selection', function () {
     it('should display the tabs and counts of annotations and notes', function () {
-      var elem = util.createDirective(document, 'selectionTabs', {
-        selectedTab: 'annotation',
-        totalAnnotations: '123',
-        totalNotes: '456',
+      var elem = util.createDirective(document, 'selectionTabs');
+
+      connectStore.updateState({
+        annotations: [ann, ann, pageNote],
       });
+      elem.scope.$digest();
 
       var tabs = elem[0].querySelectorAll('a');
 
       assert.include(tabs[0].textContent, 'Annotations');
       assert.include(tabs[1].textContent, 'Notes');
-      assert.include(tabs[0].textContent, '123');
-      assert.include(tabs[1].textContent, '456');
+      assert.include(tabs[0].textContent, '2');
+      assert.include(tabs[1].textContent, '1');
     });
 
     it('should display annotations tab as selected', function () {
-      var elem = util.createDirective(document, 'selectionTabs', {
-        selectedTab: 'annotation',
-        totalAnnotations: '123',
-        totalNotes: '456',
-      });
-
+      var elem = util.createDirective(document, 'selectionTabs');
       var tabs = elem[0].querySelectorAll('a');
       assert.isTrue(tabs[0].classList.contains('is-selected'));
     });
 
     it('should display notes tab as selected', function () {
-      var elem = util.createDirective(document, 'selectionTabs', {
-        selectedTab: 'note',
-        totalAnnotations: '123',
-        totalNotes: '456',
-      });
+      var elem = util.createDirective(document, 'selectionTabs');
+
+      connectStore.updateState({ selectedTab: 'note' });
+      elem.scope.$digest();
 
       var tabs = elem[0].querySelectorAll('a');
       assert.isTrue(tabs[1].classList.contains('is-selected'));
+    });
+
+    it('should clear the selection when a tab is clicked', function () {
+      var elem = util.createDirective(document, 'selectionTabs');
+
+      elem[0].querySelector('a').click();
+
+      assert.calledWith(connectStore.dispatch, { type: 'CLEAR_SELECTION' });
+    });
+
+    it('should select a tab when clicked', function () {
+      var elem = util.createDirective(document, 'selectionTabs');
+
+      elem[0].querySelector('a').click();
+
+      assert.calledWith(connectStore.dispatch, { type: 'SELECT_TAB', tab: 'annotation' });
     });
   });
 });
