2ac1ce9a4d0f3fd3bc8176e57d0816786eac5a25
Robert Knight <robertknight@gmail.com>
2018-04-12 15:39:07 +0100

    Replace PhantomJS with Puppeteer (Chrome Headless) in tests
    PhantomJS does not support some modern web APIs and developers had been
    experiencing problems with it consuming a lot of CPU when idle when
    using `gulp test-watch`.
    
    Puppeteer is actively maintained by the Chrome team and supports the
    modern web fully.

--- src/karma.config.js
+++ src/karma.config.js
@@ -1,8 +1,12 @@
+/* global process */
+
 'use strict';
 
 /* global __dirname */
 
 const path = require('path');
+
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function(config) {
   config.set({
@@ -137,7 +141,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless'],
     browserNoActivityTimeout: 20000, // Travis is slow...
 
     // Continuous Integration mode
