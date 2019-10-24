7579a88539bdeef3e97e3bf5284ada319ebd49cd
Robert Knight <robertknight@gmail.com>
2019-10-24 22:09:11 +0100

    Add function to convert DOM ranges to text positions
    Add a `fromRange` function which will serve as a replacement for the
    `fromRange` function in `dom-anchor-text-position`.
    
    This implementation provides better / more explicit handling of ranges that
    are entirely or partly outside of the `root` element. It acts as if the
    range's start and end positions were clamped to the boundaries of the
    root element.

--- src/annotator/anchoring/test/text-position-test.js
+++ src/annotator/anchoring/test/text-position-test.js
@@ -1,19 +1,112 @@
 'use strict';
 
-const { toRange } = require('../text-position');
+const { fromRange, toRange } = require('../text-position');
 
 describe('text-position', () => {
   let container;
+  let trailer;
 
   before(() => {
     container = document.createElement('div');
+    container.className = 'text-position-test';
     container.innerHTML = `<h1>Test article</h1>
 <p>First paragraph.</p>
 <p>Second paragraph.</p>`;
+
+    trailer = document.createElement('div');
+    trailer.className = 'text-position-trailer';
+    trailer.innerHTML = 'content after container';
+
+    document.body.appendChild(container);
+    document.body.appendChild(trailer);
   });
 
   after(() => {
     container.remove();
+    trailer.remove();
+  });
+
+  describe('fromRange', () => {
+    [
+      {
+        description: 'range at beginning of root',
+        start: ['.text-position-test > h1', 0],
+        end: ['.text-position-test > h1', 4],
+        expected: [0, 4],
+      },
+
+      {
+        description: 'range starting before root and ending within it',
+        start: ['body', null],
+        end: ['.text-position-test > h1', 4],
+        expected: [0, 4],
+      },
+
+      {
+        description: 'range starting and ending in the middle of a text node',
+        start: ['.text-position-test > h1', 5],
+        end: ['.text-position-test > h1', 8],
+        expected: [5, 8],
+      },
+
+      {
+        description: 'range starting at an element boundary',
+        start: ['.text-position-test > p', null],
+        end: ['.text-position-test > p', 5],
+        expected: [13, 18],
+      },
+
+      {
+        description: 'range ending at an element boundary',
+        start: ['.text-position-test > h1', 5],
+        end: ['.text-position-test > p', null],
+        expected: [5, 13],
+      },
+
+      {
+        description: 'range ending after root',
+        start: ['.text-position-test > h1', 5],
+        end: ['.text-position-trailer', null],
+        expected: [5, 47],
+      },
+
+      {
+        description: 'range starting and ending before root',
+        start: ['body', null],
+        end: ['body', null],
+        expected: [0, 0],
+      },
+
+      {
+        description: 'range starting and ending after root',
+        start: ['.text-position-trailer', null],
+        end: ['.text-position-trailer', null],
+        expected: [47, 47],
+      },
+    ].forEach(({ description, start, end, expected }) => {
+      it(`returns correct \`start\` and \`end\` offsets (${description})`, () => {
+        const root = container;
+        const range = container.ownerDocument.createRange();
+        const [startElement, startOffset] = start;
+        const [endElement, endOffset] = end;
+
+        let startContainer = document.querySelector(startElement);
+        if (typeof startOffset === 'number') {
+          startContainer = startContainer.childNodes[0];
+        }
+
+        let endContainer = document.querySelector(endElement);
+        if (typeof endOffset === 'number') {
+          endContainer = endContainer.childNodes[0];
+        }
+
+        range.setStart(startContainer, startOffset);
+        range.setEnd(endContainer, endOffset);
+
+        const actual = fromRange(root, range);
+        assert.deepEqual(actual, expected);
+      });
+    });
   });
 
   describe('toRange', () => {
