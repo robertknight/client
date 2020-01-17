6f08acf4a30a8264ca97a274b708ba8bea018212
Robert Knight <robertknight@gmail.com>
2020-01-17 12:24:50 +0000

    Convert `deleteNewAndEmptyDrafts` to a transaction

--- src/sidebar/store/transactions/create-annotation.js
+++ src/sidebar/store/transactions/create-annotation.js
@@ -3,6 +3,23 @@ import { defaultPermissions } from '../../util/permissions';
 import * as metadata from '../../util/annotation-metadata';
 import uiConstants from '../../ui-constants';
 
+const deleteNewAndEmptyDrafts = createTransaction(
+  'deleteNewAndEmptyDrafts',
+  store => {
+    const newDrafts = store.getState().drafts.filter(draft => {
+      return (
+        metadata.isNew(draft.annotation) &&
+        !getDraftIfNotEmpty(getState(), draft.annotation)
+      );
+    });
+    const removedAnnotations = newDrafts.map(draft => {
+      store.removeDraft(draft.annotation);
+      return draft.annotation;
+    });
+    store.removeAnnotations(removedAnnotations);
+  }
+);
+
 export default createTransaction(
   'createAnnotation',
   (store, ann, now = new Date()) => {
@@ -28,7 +45,7 @@ export default createTransaction(
 
     // When a new annotation is created, remove any existing annotations
     // that are empty.
-    store.deleteNewAndEmptyDrafts([ann]);
+    deleteNewAndEmptyDrafts.run(store, [ann]);
 
     store.addAnnotations([ann]);
 
