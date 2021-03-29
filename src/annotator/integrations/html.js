14c12b012795d9b3e3aae3e10d8c077733231dec
Robert Knight <robertknight@gmail.com>
2021-03-29 07:57:05 +0100

    WIP - Implement side-by-size mode for HTML documents
    We recently improved the experience of annotating PDF documents by
    showing the sidebar alongside the content when there is enough
    horizontal space available. This commit extends that approach to web pages,
    with modifications to allow for differences between web pages and PDFs:
    
     - Web pages do not have a known layout and response to being resized
       that we can rely on
    
     - Web pages will often have peripheral content on the side which we
       should not try to squeeze into the available space when the sidebar
       is opened. Instead we should continue to allow the sidebar to cover
       it up to make more room for the main content
    
    This is implemented by first shrinking the `<body>` by the full width of
    the sidebar when it opens and then increasing the `<body>`'s width if
    there is space between the "main content" of the page and the edge of
    the sidebar. Whatever is in this space (eg. related stories, ads) will
    then be covered up by the sidebar.
    
    Determining what consitutes the "main content" of the page
    is done by heuristics in `guessMainContentArea`. These heuristics assume
    that the main content can be inferred by considering the longest
    paragraphs of text on the page and looking for common vertical margins
    amongst them.
    
    Resizing the document will generally cause the content to shift. To try
    and ensure the same content remains visible before and after opening the
    sidebar, a `preserveScrollPosition` helper attempts to determine how
    much the content shifted after the relayout and adjust the scroll
    position to keep, near-enough, the same content visible. To do this, it
    chooses an element near the top of the screen as an anchor and checks
    how much it shifts following the resize. It then adjusts the document's
    scroll position to compensate.

--- src/annotator/integrations/html.js
+++ src/annotator/integrations/html.js
@@ -1,12 +1,14 @@
 import scrollIntoView from 'scroll-into-view';
 
 import { anchor, describe } from '../anchoring/html';
+import { guessMainContentArea, preserveScrollPosition } from '../util/document-resizing';
 
 import { HTMLMetadata } from './html-metadata';
 
 /**
  * @typedef {import('../../types/annotator').Anchor} Anchor
  * @typedef {import('../../types/annotator').Integration} Integration
+ * @typedef {import('../../types/annotator').SidebarLayout} SidebarLayout
  */
 
 /**
@@ -34,9 +36,96 @@ export class HTMLIntegration {
     return this.container;
   }
 
-  fitSideBySide() {
-    // Not yet implemented.
-    return false;
+  /**
+   * @param {SidebarLayout} layout
+   */
+  fitSideBySide(layout) {
+    if (layout.expanded) {
+      this._activateSideBySide(layout.width);
+      return true;
+    } else {
+      this._deactivateSideBySide();
+      return false;
+    }
+  }
+
+  /**
+   * Resize the document content after side-by-side mode is activated.
+   *
+   * Subclasses can override this to customize how the document viewer is
+   * resized when side-by-side mode is activated. If they do, they should also
+   * implement `deactivateSideBySide`.
+   *
+   * @param {number} sidebarWidth
+   */
+  _activateSideBySide(sidebarWidth) {
+    // When side-by-side mode is activated, what we want to achieve is that the
+    // main content of the page is fully visible alongside the sidebar, with
+    // as much space given to the main content as possible. A challenge is that
+    // we don't know how the page will respond to reducing the width of the body.
+    //
+    // - The content might have margins which automatically get reduced as the
+    //   available width is reduced. For example a blog post with a fixed-width
+    //   article in the middle and `margin: auto` for both margins.
+    //
+    //   In this scenario we'd want to reduce the document width by the full
+    //   width of the sidebar.
+    //
+    // - There might be sidebars to the left and/or right of the main content
+    //   which cause the main content to be squashed when the width is reduced.
+    //   For example a news website with a column of ads on the right.
+    //
+    //   In this scenario we'd want to not reduce the document width or reduce
+    //   it by a smaller amount and let the Hypothesis sidebar cover up the
+    //   document's sidebar, leaving as much space as possible to the content.
+    //
+    // Therefore what we do is to initially reduce the width of the document by
+    // the full width of the sidebar, then we use heuristics to analyze the
+    // resulting page layout and determine whether there is significant "free space"
+    // (ie. anything that is not the main content of the document, such as ads or
+    // links to related stories) to the right of the main content. If there is,
+    // we make the document wider again to allow more space for the main content.
+    //
+    // These heuristics assume a typical "article" page with one central block
+    // of content. If we can't find the "main content" then we just assume that
+    // everything on the page is potentially content that the user might want
+    // to annotate and so try to keep it all visible.
+    const padding = 10;
+    const width = window.innerWidth - sidebarWidth;
+    const rightMargin = window.innerWidth - width + padding;
+
+    preserveScrollPosition(() => {
+      // FIXME: If there is a CSS rule that sets the `width` of the body, that
+      // will override this margin. eg. There are various sites that have
+      // `width: 100%` rules for the body.
+      document.body.style.marginRight = `${rightMargin}px`;
+
+      const contentArea = guessMainContentArea(document.body);
+      if (contentArea) {
+        // Check if we can give the main content more space by letting the
+        // sidebar overlap stuff in the document to the right of the main content.
+        const freeSpace = window.innerWidth - sidebarWidth - contentArea.right;
+        if (freeSpace > 200) {
+          document.body.style.marginRight = `${rightMargin - freeSpace}px`;
+        }
+
+        // If the main content appears to be right up against the edge of the
+        // window, add padding for readability.
+        if (contentArea.left < 10) {
+          document.body.style.marginLeft = `${padding}px`;
+        }
+      }
+    });
+  }
+
+  /**
+   * Undo the effects of `activateSideBySide`.
+   */
+  _deactivateSideBySide() {
+    preserveScrollPosition(() => {
+      document.body.style.marginLeft = '';
+      document.body.style.marginRight = '';
+    });
   }
 
   async getMetadata() {
