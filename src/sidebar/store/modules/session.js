418d09a8b62d18851eca84ce63eef34ad6fb4c45
Robert Knight <robertknight@gmail.com>
2021-05-30 15:07:27 +0100

    Convert `session` module to `createStoreModule`

--- src/sidebar/store/modules/session.js
+++ src/sidebar/store/modules/session.js
@@ -1,6 +1,4 @@
-import * as util from '../util';
-
-import { storeModule } from '../create-store';
+import { createStoreModule } from '../create-store';
 
 /**
  * @typedef {import('../../../types/api').Profile} Profile
@@ -23,7 +21,7 @@ const initialProfile = {
   userid: null,
 };
 
-function init(settings) {
+function initialState(settings) {
   return {
     /**
      * The app's default authority (user identity provider), from settings,
@@ -41,89 +39,54 @@ function init(settings) {
   };
 }
 
-const update = {
-  UPDATE_PROFILE: function (state, action) {
-    return {
-      profile: { ...action.profile },
-    };
-  },
-};
-
-const actions = util.actionTypes(update);
-
-/**
- * Update the profile information for the current user.
- */
-function updateProfile(profile) {
-  return {
-    type: actions.UPDATE_PROFILE,
-    profile,
-  };
-}
-
-/**
- *
- * @return {string}
- */
-function defaultAuthority(state) {
-  return state.defaultAuthority;
-}
-
-/**
- * Return true if a user is logged in and false otherwise.
- *
- * @param {object} state - The application state
- */
-function isLoggedIn(state) {
-  return state.profile.userid !== null;
-}
-
-/**
- * Return true if a given feature flag is enabled for the current user.
- *
- * @param {object} state - The application state
- * @param {string} feature - The name of the feature flag. This matches the
- *        name of the feature flag as declared in the Hypothesis service.
- */
-function isFeatureEnabled(state, feature) {
-  return !!state.profile.features[feature];
-}
-
-/**
- * Return true if the user's profile has been fetched. This can be used to
- * distinguish the dummy profile returned by `profile()` on startup from a
- * logged-out user profile returned by the server.
- */
-function hasFetchedProfile(state) {
-  return state.profile !== initialProfile;
-}
-
-/**
- * Return the user's profile.
- *
- * Returns the current user's profile fetched from the `/api/profile` endpoint.
- *
- * If the profile has not yet been fetched yet, a dummy logged-out profile is
- * returned. This allows code to skip a null check.
- */
-function profile(state) {
-  return state.profile;
-}
-
-export default storeModule({
-  init,
+export default createStoreModule(initialState, {
   namespace: 'session',
-  update,
 
   actions: {
-    updateProfile,
+    /** @param {Profile} profile */
+    updateProfile(state, profile) {
+      return { profile };
+    },
   },
 
   selectors: {
-    defaultAuthority,
-    hasFetchedProfile,
-    isFeatureEnabled,
-    isLoggedIn,
-    profile,
+    defaultAuthority(state) {
+      return state.defaultAuthority;
+    },
+
+    /**
+     * Return true if the user's profile has been fetched. This can be used to
+     * distinguish the dummy profile returned by `profile()` on startup from a
+     * logged-out user profile returned by the server.
+     */
+    hasFetchedProfile(state) {
+      return state.profile !== initialProfile;
+    },
+
+    /**
+     * Return true if a given feature flag is enabled for the current user.
+     *
+     * @param {string} feature - The name of the feature flag. This matches the
+     *        name of the feature flag as declared in the Hypothesis service.
+     */
+    isFeatureEnabled(state, feature) {
+      return !!state.profile.features[feature];
+    },
+
+    isLoggedIn(state) {
+      return state.profile.userid !== null;
+    },
+
+    /**
+     * Return the user's profile.
+     *
+     * Returns the current user's profile fetched from the `/api/profile` endpoint.
+     *
+     * If the profile has not yet been fetched yet, a dummy logged-out profile is
+     * returned. This allows code to skip a null check.
+     */
+    profile(state) {
+      return state.profile;
+    },
   },
 });
